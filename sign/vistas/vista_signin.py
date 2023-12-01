2799c36ff2ee891dc047f0461f659ecd7131a491
danierazome <j.erao@uniandes.edu.co>
2023-12-01 14:20:30 -0500

    feature: endpoint of auth server was modified on sign up service

--- sign/vistas/vista_signin.py
+++ sign/vistas/vista_signin.py
@@ -1,7 +1,7 @@
 from flask import request
 from flask_restful import Resource
 from modelos import Usuario
-from constant import INTERNAL_LOAD_BALANCER, GENERATE_TOKEN_ENDPOINT
+from constant import AUTH_SERVER_ENDPOINT, GENERATE_TOKEN_ENDPOINT
 
 import requests
 
@@ -16,7 +16,7 @@ class VistaLogin(Resource):
 
         if usuario.password == request.json['password']:
             response_generar_token = requests.post(
-                url=f'{INTERNAL_LOAD_BALANCER}{GENERATE_TOKEN_ENDPOINT}',
+                url=f'{AUTH_SERVER_ENDPOINT}{GENERATE_TOKEN_ENDPOINT}',
                 json={"user_id": usuario.id})
 
             return response_generar_token.json()
@@ -24,4 +24,4 @@ class VistaLogin(Resource):
         return {"mensaje": "Password incorrecto"}, 400
 
     def get(self):
-        return {"url": f'{INTERNAL_LOAD_BALANCER}{GENERATE_TOKEN_ENDPOINT}'}
+        return {"url": f'{AUTH_SERVER_ENDPOINT}{GENERATE_TOKEN_ENDPOINT}'}
