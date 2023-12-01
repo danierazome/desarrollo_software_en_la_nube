a80a7c98493350c6ab4cae54f161c1a4fe17d097
danierazome <j.erao@uniandes.edu.co>
2023-12-01 16:57:29 -0500

    feature: get methods were changed to post on MANAGE-CONVERSION SERVICE

--- manage-conversion/vistas/vista_conversion.py
+++ manage-conversion/vistas/vista_conversion.py
@@ -4,10 +4,9 @@ from modelos import db, VideoConversion
 from .gcp_storage import delete_video
 
 
-
 class VistaVideoConversion(Resource):
 
-    def get(self):
+    def post(self):
         conversion = VideoConversion.query.filter_by(
             id=request.json['conversion_id'], usuario_id=request.json['usuario_id']).first()
 
@@ -18,19 +17,19 @@ class VistaVideoConversion(Resource):
             return {"mensaje": "Conversion se encuentra en proceso"}
 
         return {
-                    'id': conversion.id,
-                    'video': conversion.video,
-                    'video_name': conversion.video_name,
-                    'original_format': conversion.original_format,
-                    'video_converted': conversion.video_converted,
-                    'conversion_format': conversion.conversion_format,
-                    'state': conversion.state
-                }
+            'id': conversion.id,
+            'video': conversion.video,
+            'video_name': conversion.video_name,
+            'original_format': conversion.original_format,
+            'video_converted': conversion.video_converted,
+            'conversion_format': conversion.conversion_format,
+            'state': conversion.state
+        }
 
 
 class VistaVideoConversions(Resource):
 
-    def get(self):
+    def post(self):
         usuario_id = request.json['usuario_id']
         conversions = VideoConversion.query.filter_by(
             usuario_id=usuario_id).all()
@@ -57,13 +56,13 @@ class VistaDeleteVideoConversion(Resource):
     def delete(self):
         id = request.json['id']
         usuario_id = request.json['usuario_id']
-        
+
         conversion = VideoConversion.query.filter_by(
             id=id, usuario_id=usuario_id).first()
 
         if conversion is None:
             return {'mensaje': 'No se encontró la conversión'}, 404
-        
+
         if conversion.state != "DONE":
             return {"mensaje": "Conversion en proceso no puede ser borrada"}
 
@@ -83,8 +82,6 @@ class VistaDeleteVideoConversion(Resource):
         db.session.commit()
 
         return {'mensaje': 'La conversión se ha eliminado exitosamente'}
-       
-    
+
     def get_video_name(self, id, name, format):
         return f'{id}{name}.{format}'
-
